70db7093777bcfb8c238d8678ef4f124311b4c33
Theo Julienne <theo.julienne@gmail.com>
2009-12-29 01:01:08 +0000

    fix oops in uart lib, example shouldn't flood uart

--- penguino-lib/example.c
+++ penguino-lib/example.c
@@ -5,75 +5,78 @@
 #include "penguino/io.h"
 #include "penguino/time.h"
 
-#define UART_BAUD_RATE 500000
+#define UART_BAUD_RATE 115200
 
 #define LOW_READING  341
 #define HIGH_READING 682
 
 int main( void ) {
-   // initalise UART
-   uart_init( UART_BAUD_RATE );
-   
-   // enable UART as stdio
-   uart_stdio_init( );
-   
-   printf( "Hello World\r\n" );
+	// initalise UART
+	uart_init( UART_BAUD_RATE );
+	
+	// enable UART as stdio
+	uart_stdio_init( );
+	
+	printf( "Hello World\r\n" );
 
-   
-   // flash status LED
-   statusLed_init( );
-   
-   statusLed_red( );
-   delay_ms( 300 );
-   
-   statusLed_orange( );
-   delay_ms( 300 );
-   
-   statusLed_green( );
-   delay_ms( 300 );
-   
-   statusLed_off( );
-   delay_ms( 300 );
-   
-   
-   // Port B pin 0 is an output
-   output_init( B, 0 );
-   
-   // Port B pin 1 is an input with a pull-up
-   pullupInput_init( B, 1 );
-   
-   // initialise ADC
-   adc_init( );
-   
-   // port A pin 0 is for ADC reading
-   adcInput_init( 0 );
-   
-   // take ADC readings
-   while( 1 ) {
-      // read ADC reading on port A, pin 0
-      uint16_t potReading = adc_read( 0 );
-      
-      if ( potReading < LOW_READING ) {
-         statusLed_green( );
-      } else if ( potReading > HIGH_READING ) {
-         statusLed_red( );
-      } else {
-         // in the middle
-         statusLed_orange( );
-      }
-      
-      printf( "ADC Reading: %d\r\n", potReading );
-      
-      if ( readPin( B, 1 ) ) {
-         printf( "Port B Pin 1 Active\r\n" );
-         
-         // drive B0 high whenever B1 is high
-         drivePin( B, 0, high );
-      } else {
-         
-         drivePin( B, 0, low );
-      }
-   }
+	
+	// flash status LED
+	statusLed_init( );
+	
+	statusLed_red( );
+	delay_ms( 300 );
+	
+	statusLed_orange( );
+	delay_ms( 300 );
+	
+	statusLed_green( );
+	delay_ms( 300 );
+	
+	statusLed_off( );
+	delay_ms( 300 );
+	
+	
+	// Port B pin 0 is an output
+	output_init( B, 0 );
+	
+	// Port B pin 1 is an input with a pull-up
+	pullupInput_init( B, 1 );
+	
+	// initialise ADC
+	adc_init( );
+	
+	// port A pin 0 is for ADC reading
+	adcInput_init( 0 );
+	
+	// take ADC readings
+	while( 1 ) {
+		// read ADC reading on port A, pin 0
+		uint16_t potReading = adc_read( 0 );
+		
+		if ( potReading < LOW_READING ) {
+			statusLed_green( );
+		} else if ( potReading > HIGH_READING ) {
+			statusLed_red( );
+		} else {
+			// in the middle
+			statusLed_orange( );
+		}
+		
+		printf( "ADC Reading: %d\r\n", potReading );
+		
+		if ( readPin( B, 1 ) ) {
+			printf( "Port B Pin 1 Active\r\n" );
+			
+			// drive B0 high whenever B1 is high
+			drivePin( B, 0, high );
+		} else {
+			
+			drivePin( B, 0, low );
+		}
+        
+        // sleep for a bit to avoid flooding UART
+        delay_ms( 300 );
+	}
 
-   return 0;
+	return 0;
 }
--- penguino-lib/penguino/uart/uart.c
+++ penguino-lib/penguino/uart/uart.c
@@ -1,39 +1,39 @@
 /************************************************************************
-Title:    UART for the Penguino AVR
-Author:   David Collien (Icy Labs)
+Title:	 UART for the Penguino AVR
+Author:	David Collien (Icy Labs)
 Software: AVR-GCC 4.1, AVR Libc 1.4
 Hardware: Penguino AVR
 License:  MIT License
 
 DESCRIPTION:
-    Interface for initialising, sending and receiving data over UART
-    
+	 Interface for initialising, sending and receiving data over UART
+	 
 LICENSE:
-    Copyright (C) 2009 Icy Labs Pty Ltd.
-    http://www.icy.com.au/
+	 Copyright (C) 2009 Icy Labs Pty Ltd.
+	 http://www.icy.com.au/
 
-     Permission is hereby granted, free of charge, to any person
-     obtaining a copy of this software and associated documentation
-     files (the "Software"), to deal in the Software without
-     restriction, including without limitation the rights to use,
-     copy, modify, merge, publish, distribute, sublicense, and/or sell
-     copies of the Software, and to permit persons to whom the
-     Software is furnished to do so, subject to the following
-     conditions:
+	  Permission is hereby granted, free of charge, to any person
+	  obtaining a copy of this software and associated documentation
+	  files (the "Software"), to deal in the Software without
+	  restriction, including without limitation the rights to use,
+	  copy, modify, merge, publish, distribute, sublicense, and/or sell
+	  copies of the Software, and to permit persons to whom the
+	  Software is furnished to do so, subject to the following
+	  conditions:
 
-     The above copyright notice and this permission notice shall be
-     included in all copies or substantial portions of the Software.
+	  The above copyright notice and this permission notice shall be
+	  included in all copies or substantial portions of the Software.
 
-     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-     OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-     HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-     WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-     OTHER DEALINGS IN THE SOFTWARE.
+	  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	  OTHER DEALINGS IN THE SOFTWARE.
 
-    
+	 
 ************************************************************************/
 
 #include "../common.h"
@@ -44,7 +44,7 @@ LICENSE:
 #include "uart.h"
  
 #define UART_STATUS  UCSRA
-#define UART_DATA    UDR
+#define UART_DATA	 UDR
 #define UART_CONTROL UCSRB
 
 
@@ -54,66 +54,67 @@ static volatile unsigned char lastRxError;
 
 
 // Receive from UART
-SIGNAL( SIG_USART_RECV ) {
-   unsigned char data = UART_DATA;
-   
-   lastRxError = UART_STATUS & (_BV( FE ) | _BV( DOR ));
-   
-   Buffer_StoreElement( &UARTInputBuffer, data );
+ISR( USART_RXC_vect ) {
+	unsigned char data = UART_DATA;
+	
+	lastRxError = UART_STATUS & (_BV( FE ) | _BV( DOR ));
+	
+	Buffer_StoreElement( &UARTInputBuffer, data );
 }
 
 // Send over UART
-SIGNAL( SIG_USART_DATA ) {
-   
-   // check if there's something to send
-   if ( UARTOutputBuffer.Elements > 0 ) {
-      UART_DATA = Buffer_GetElement( &UARTOutputBuffer );
-   } else {
-      // disable the sending interrupt
-      UART_CONTROL &= ~_BV( UDRIE );
-   }
+ISR( USART_UDRE_vect ) {
+	
+	// check if there's something to send
+	if ( UARTOutputBuffer.Elements > 0 ) {
+		UART_DATA = Buffer_GetElement( &UARTOutputBuffer );
+	} else {
+		// disable the sending interrupt
+		UART_CONTROL &= ~_BV( UDRIE );
+	}
 }
 
 void uart_init( uint32_t baudrate ) {
-   
-   unsigned int scaledBaud = ((F_CPU/16)/baudrate)-1;
-   
-   // enable interrupts
-   sei( );
-   
-   // initialise buffers for sending and receiving data
-   Buffer_Initialize( &UARTInputBuffer );
-   Buffer_Initialize( &UARTOutputBuffer );
-   
-   // set baud rate
-   UBRRH = (unsigned char)(scaledBaud >> 8);
-   UBRRL = (unsigned char) scaledBaud;
-   
-   // enable uart interrupts and start interrupting for receiving
-   UART_CONTROL = _BV( RXCIE ) | (1 << RXEN) | (1 << TXEN);
-   
-   // set the frame format
-   UCSRC = (3 << UCSZ0);
+	
+	unsigned int scaledBaud = ((F_CPU/16)/baudrate)-1;
+	
+	// enable interrupts
+	sei( );
+	
+	// initialise buffers for sending and receiving data
+	Buffer_Initialize( &UARTInputBuffer );
+	Buffer_Initialize( &UARTOutputBuffer );
+	
+	// set baud rate
+	UBRRH = (unsigned char)(scaledBaud >> 8);
+	UBRRL = (unsigned char) scaledBaud;
+	
+	// enable uart interrupts and start interrupting for receiving
+	UART_CONTROL = _BV( RXCIE ) | (1 << RXEN) | (1 << TXEN);
+	
+	// set the frame format
+	// URSEL bit selects the UCSRC register as the target
+	UCSRC = (1 << URSEL) | (3 << UCSZ0);
 }
 
 void uart_putc( unsigned char data ) {
-   Buffer_StoreElement( &UARTOutputBuffer, data );
-   
-   // enable the sending interrupt
-   UART_CONTROL |= _BV( UDRIE );
+	Buffer_StoreElement( &UARTOutputBuffer, data );
+	
+	// enable the sending interrupt
+	UART_CONTROL |= _BV( UDRIE );
 }
 
 
 void uart_puts( const char *s ) {
-   while ( *s ) {
-      uart_putc(*s++);
-   }
+	while ( *s ) {
+		uart_putc(*s++);
+	}
 }
 
 bool uart_hasData( void ) {
-   return (UARTInputBuffer.Elements > 0);
+	return (UARTInputBuffer.Elements > 0);
 }
 
-unsigned char uart_getc( void ) {    
-   return Buffer_GetElement( &UARTInputBuffer );
+unsigned char uart_getc( void ) {	 
+	return Buffer_GetElement( &UARTInputBuffer );
 }
